dca8a437619db05e367f71839530e7749aeab9ea
jon <jonathan.garrett@gmail.com>
2016-12-09 16:31:39 -0800

    Create file for move zeros to left problem

---
+++ moveZeros.js
@@ -0,0 +1,5 @@
+/*
+Given an integer array, move all elements containing 0 to the left while maintaining the order of other element.
+[1, 10, 20, 0, 59, 63, 0, 88, 0] --> [0, 0, 0, 1, 10, 20, 59, 63, 88]
+*/
+
